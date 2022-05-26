7f084df86fd3417fa8ff4a14290e303787737679
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-05-26 15:37:34 -0400

    refactor: Simplified Get Method in Favorites Views

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -4,17 +4,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from favorites.models import Favorite
 from favorites.serializers import FavoriteSerializer
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def favorite_views(request):
     if request.method == "GET":
-        try:
-            favorites = Favorite.objects.filter(user=request.user)
-        except ObjectDoesNotExist:
-            return Response({"No favorites found for that user."}, status=status.HTTP_204_NO_CONTENT)
+        favorites = Favorite.objects.filter(user=request.user)
         serializer = FavoriteSerializer(favorites, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "POST":
