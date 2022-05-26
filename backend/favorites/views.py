7ad902b6f0dd22d65aee87fc6a1b0e89d261b17b
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-05-26 12:07:44 -0400

    chore: Added Get Method to Views for Favorites + Added URL to Reach Get Method

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from favorites.models import Favorite
+from favorites.serializers import FavoriteSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def favorite_views(request):
+    if request.method == "GET":
+        try:
+            favorites = Favorite.objects.filter(user=request.user)
+        except ObjectDoesNotExist:
+            return Response({"error":"No favorites found for that user."})
+        serializer = FavoriteSerializer(favorites, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
