d09f5d48f6e73bb3cd078d1251f7acba62fe0ced
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-05-27 16:09:20 -0400

    chore: Created Axios Function on Home Page to Generate Custom API Access Token When User Loads Website for PetFinder API

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,40 +1,38 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
+import { KEY } from "../../local_key";
+import { ID } from "../../local_id";
 
 const HomePage = () => {
-  // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
-  // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+
+  const [user] = useAuth();
+  const [accessToken, setAccessToken] = useState("");
 
   useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]);
+    getAuth();
+  }, []);
+
+  const getAuth = async () => {
+    try {
+      let response = await axios.post(`https://api.petfinder.com/v2/oauth2/token`, `grant_type=client_credentials&client_id=${ID}&client_secret=${KEY}`, {
+        headers: {
+          'Content-Type': `application/x-www-form-urlencoded`
+        }
+      });
+      setAccessToken(response.data.access_token)
+      console.log(accessToken)
+    }
+    catch(error){
+      console.log(error.message)
+    }
+  };
+
+
+
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
     </div>
   );
 };
