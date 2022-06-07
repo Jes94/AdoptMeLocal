92d9ec083dc08235b0b153a59ec45d8525ab4712
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-06-07 09:37:34 -0400

    chore: Updated Favorites Model + Serializer Due to PetFinder API Restraints

--- backend/favorites/models.py
+++ backend/favorites/models.py
@@ -6,3 +6,13 @@ from authentication.models import User
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     animal_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    house_trained = models.CharField(max_length=255)
+    picture = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    age = models.CharField(max_length=255)
+    neutered = models.CharField(max_length=255)
+    kids = models.CharField(max_length=255)
+    dogs = models.CharField(max_length=255)
+    cats = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
--- backend/favorites/serializers.py
+++ backend/favorites/serializers.py
@@ -4,5 +4,5 @@ from .models import Favorite
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
-        fields = ["id", "user", "animal_id"]
+        fields = ["id", "user", "animal_id","name","picture","breed","age","house_trained","neutered","kids","dogs","cats","contact"]
         depth = 1
