c7dc2d486a64aeb3189f8700b0b1c3d1125a2849
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-07-19 16:37:02 -0400

    improvement: Only Displaying Animals on Results Page That Have Descriptions + Reduced Code in ListAnimals

--- frontend/src/components/ListAnimals/ListAnimals.jsx
+++ frontend/src/components/ListAnimals/ListAnimals.jsx
@@ -19,8 +19,7 @@ const ListAnimals = ({animals, getDetails}) => {
 
     return(
         animals.map((animal) => {
-            if (animal.photos[0] !== undefined) {
-                if (animal.description !== null){
+            if (animal.photos[0] !== undefined && animal.description !== null) {
                     const charEncode = /&#39;|&amp;#39;|&#039;/g
                     const description = animal.description.replace(charEncode, "'")
                     const name = animal.name.replace(charEncode, "'")
@@ -34,17 +33,6 @@ const ListAnimals = ({animals, getDetails}) => {
                         <Card.Footer><button className="btn btn-primary btn-sm" key={animal.id} id={animal.id} onClick={handleSubmit} style={{background: "#008000", border:"#008000"}}>More Info</button></Card.Footer>
                         </Card>
                     )
-                }
-                return ( 
-                    <Card style={{ width: '18rem', height: '21rem'}} key={animal.id}>
-                        <Card.Img varient="top" src={animal.photos[0].full} style={{width:'9rem', height:'9rem', marginLeft:'4.5rem'}} alt={animal.name}/>
-                        <Card.Body>
-                            <Card.Title style={{overflow:'hidden', 'fontSize':'medium'}}>{animal.name}</Card.Title>
-                            <Card.Text style={{overflow:'hidden', 'fontSize':'smaller'}}>{animal.description}</Card.Text>
-                        </Card.Body>
-                        <Card.Footer><button className="btn btn-primary btn-sm" key={animal.id} id={animal.id} onClick={handleSubmit} style={{background: "#008000", border:"#008000"}}>More Info</button></Card.Footer>
-                    </Card>
-                )
             }
             else{
                 return null;
